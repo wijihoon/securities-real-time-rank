5d65325370e3ac4b2718c8e2885abb41718d0c05
위지훈 <wijihoon123@naver.com>
2022-09-26 14:17:22 +0900

    Update ExceptionMessage.java

--- src/main/java/com/kakaopaysec/exception/ExceptionMessage.java
+++ src/main/java/com/kakaopaysec/exception/ExceptionMessage.java
@@ -14,7 +14,7 @@ public class ExceptionMessage {
     public Map<String, String> errMsg(String msg, String errCode){
         Map<String, String> result = new HashMap<>();
         result.put("code", errCode);
-        result.put("msg", msg);
+        result.put("message", msg);
         return result;
 
     }
